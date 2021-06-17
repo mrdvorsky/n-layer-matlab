73123f31174b9ac53a1e34b9d5f0b4cd990dd76b
Matthew Dvorsky <mdvorsky@iastate.edu>
2021-06-17 18:26:28 -0500

    Added script to add folder to path.

---
+++ MatlabAddToPathScript.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET startupHeader=%%%% Amntl Matlab Utilities
+
+SET scriptPath=%~dp0
+SET startupPath=C:\Users\%username%\Documents\MATLAB\startup.m
+SET copyString=addpath(genpath("%scriptPath%"));
+
+
+IF EXIST %startupPath% (
+	FIND /c "addpath(genpath(""%scriptPath%""))" %startupPath% > NUL
+	IF !errorlevel! EQU 0 (
+		MSG "%username%" Already installed.
+	) ELSE (
+		CALL :performInstallation
+	)
+) ELSE (
+	CALL :performInstallation
+)
+
+EXIT /B %errorlevel%
+:performInstallation
+	ECHO.>>%startupPath%
+	ECHO.>>%startupPath%
+	ECHO %startupHeader%>>%startupPath%
+	ECHO %copyString%>>%startupPath%
+	ECHO | SET /p="%copyString%" | clip
+	MSG "%username%" Installation successful. Restart Matlab or run the following command (automatically copied to clipboard): %copyString%

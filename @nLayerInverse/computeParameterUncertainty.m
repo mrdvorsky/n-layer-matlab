a87b1c25b66a694bdf962e303b0deb5524ba798b
Matthew Dvorsky <mdvorsky@iastate.edu>
2022-11-21 10:29:10 -0600

    Updated computeParameterUncertainty for refactoring. Now returns an uncertainty struct.

--- @nLayerInverse/computeParameterUncertainty.m
+++ @nLayerInverse/computeParameterUncertainty.m
@@ -1,4 +1,4 @@
-function [xUncertainty] = computeParameterUncertainty(O, NL, f, options)
+function [Uncertainty] = computeParameterUncertainty(O, NL, f, options)
 %COMPUTEPARAMETERUNCERTAINTY Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -16,22 +16,31 @@ arguments
 end
 
 %% Construct Linearized Ranges and Initial Guesses
+O.validate();
 [xInitial, ~, ~] = O.constructInitialValuesAndRanges();
 
-[~, gam] = O.calculateError(xInitial, NL, f, num2cell(zeros(length(NL), 1)));
+[~, gam] = O.calculateError({O}, xInitial, NL, f, num2cell(zeros(length(NL), 1)));
 
 %% Create Error Function
-errorFunctionVector = @(x) O.calculateError(x, NL, f, gam);
+errorFunctionVector = @(x) O.calculateError({O}, x, NL, f, gam);
 
 %% Calculate Jacobian
 [~, ~, ~, ~, ~, ~, J]  = lsqnonlin(errorFunctionVector, xInitial, [], [], ...
     optimoptions(O.localOptimizerOptions, Display="none"));
 
-% svd(full(J).' * full(J))
-xUncertainty = inv(full(J).' * full(J)) .* 0.5 * options.NoiseStd.^2;
-% xUncertainty = (pinv(full(J)) * pinv(full(J)).') .* 0.5 .* options.NoiseStd.^2;
+%% Compute Uncertainty from Jacobian
+hess = inv(full(J).' * full(J)) .* 0.5 * options.NoiseStd.^2;
+xUncertainty = sqrt(diag(hess));
 
-disp(sqrt(diag(xUncertainty)));
+[er, ur, thk] = O.extractStructure(xInitial, f);
+[Uncertainty.erLower, Uncertainty.urLower, Uncertainty.thkLower] = ...
+    O.extractStructure(xInitial - xUncertainty, f);
+[Uncertainty.erUpper, Uncertainty.urUpper, Uncertainty.thkUpper] = ...
+    O.extractStructure(xInitial + xUncertainty, f);
+
+Uncertainty.er  = max(abs(er  - Uncertainty.erLower),  abs(er  - Uncertainty.erUpper));
+Uncertainty.ur  = max(abs(ur  - Uncertainty.urLower),  abs(ur  - Uncertainty.urUpper));
+Uncertainty.thk = max(abs(thk - Uncertainty.thkLower), abs(thk - Uncertainty.thkUpper));
 
 end
 

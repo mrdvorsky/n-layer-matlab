a6c8eacba54c10dafd6e3d9946ac945aa713dd56
Matthew Dvorsky <matthew.ryan.dvorsky@gmail.com>
2025-05-01 17:50:57 -0500

    Added a showMode function to the waveguideMode class, for visualizing the waveguide modes.

--- source/common/waveguide-modes/+nLayer/@waveguideMode/showMode.m
+++ source/common/waveguide-modes/+nLayer/@waveguideMode/showMode.m
@@ -10,8 +10,11 @@ arguments
 
     options.SizeX(1, 1) {mustBePositive};
     options.SizeY(1, 1) {mustBePositive};
-    options.NumPointsX(1, 1) {mustBePositive, mustBeInteger} = 625;
-    options.NumPointsY(1, 1) {mustBePositive, mustBeInteger} = 625;
+    options.NumPointsX(1, 1) {mustBePositive, mustBeInteger} = 500;
+    options.NumPointsY(1, 1) {mustBePositive, mustBeInteger} = 500;
+
+    options.ArrowDecimationFactorX(1, 1) {mustBePositive, mustBeInteger} = 10;
+    options.ArrowDecimationFactorY(1, 1) {mustBePositive, mustBeInteger} = 10;
 end
 
 %% Check Size
@@ -34,13 +37,36 @@ kphi = atan2(ky, kx);
 ExHat = O.ExSpec(kx, ky, kr, kphi);
 EyHat = O.EySpec(kx, ky, kr, kphi);
 
-scaleFactor = numel(ExHat) * abs(kx(1) - kx(2)) * abs(ky(1) - ky(2)) ...
+intScaleFactor = numel(kr) * abs(kx(1) - kx(2)) * abs(ky(1) - ky(2)) ...
     ./ (2*pi);
-Ex = fftshift(ifft2(ifftshift(ExHat))) * scaleFactor;
-Ey = fftshift(ifft2(ifftshift(EyHat))) * scaleFactor;
 
-%% Plot Fields
-showImage(x, y, sqrt(Ex.^2 + Ey.^2), DisplayFormat="Magnitude");
+Ex = fftshift(ifft2(ifftshift(ExHat))) * intScaleFactor;
+Ey = fftshift(ifft2(ifftshift(EyHat))) * intScaleFactor;
+
+%% Plot Field Magnitudes
+Er = hypot(Ex, Ey);
+showImage(x, y, Er, DisplayFormat="Magnitude");
+
+%% Plot Vectors
+dx = abs(x(2) - x(1));
+dy = abs(y(2) - y(1));
+decX = options.ArrowDecimationFactorX;
+decY = options.ArrowDecimationFactorY;
+
+arrowScaleFactor = 0.7*max(dx*decX, dy*decY) ./ max(Er(:));
+uq = arrowScaleFactor * real(Ex);
+vq = arrowScaleFactor * real(Ey);
+
+xq = x - 0.5*uq;
+yq = y - 0.5*vq;
+
+xq = xq(1:decX:end, 1:decY:end);
+yq = yq(1:decX:end, 1:decY:end);
+vq = vq(1:decX:end, 1:decY:end);
+uq = uq(1:decX:end, 1:decY:end);
+
+hold on;
+quiver(xq(:), yq(:), uq(:), vq(:), "off", "k");
 
 end
 

70e56c0a238396e5da05c540ab0378b191333496
Matthew Dvorsky <matthew.ryan.dvorsky@gmail.com>
2025-05-23 22:41:21 -0500

    Centered polar plot. Fixed export and copy menu functionality, which was broken due to refactor.

--- source/vizualization/private/setupCopyExportMenu.m
+++ source/vizualization/private/setupCopyExportMenu.m
@@ -1,4 +1,4 @@
-function [] = setupCopyExportMenu(fig)
+function [] = setupCopyExportMenu(fig, plotAxis, structureAxis)
 %Sets up the menu bar for copying and exporting the figure.
 % This is for nLayerViewer.
 % 
@@ -6,6 +6,8 @@ function [] = setupCopyExportMenu(fig)
 
 arguments
     fig(1, 1) matlab.ui.Figure;
+    plotAxis(1, 1) matlab.graphics.axis.Axes;
+    structureAxis(1, 1) matlab.graphics.axis.Axes;
 end
 
 %% Create Menu Bar Entry
@@ -14,71 +16,77 @@ copyMenu = uimenu(fig, Text="Copy/Export Figure");
 %% Add Items
 uimenu(copyMenu, ...
     Text="Copy Polar Plot", ...
-    MenuSelectedFcn=@copyFigure);
+    MenuSelectedFcn={@copyPolarPlot, plotAxis});
 uimenu(copyMenu, ...
     Text="Copy Structure Definition", ...
-    MenuSelectedFcn=@copyStructure);
+    MenuSelectedFcn={@copyStructure, structureAxis});
 uimenu(copyMenu, ...
     Text="Copy Both", ...
-    MenuSelectedFcn=@copyFigureAndStructure);
+    MenuSelectedFcn={@copyPolarPlotAndStructure, plotAxis});
 
 uimenu(copyMenu, ...
     Text="Export Polar Plot", ...
-    MenuSelectedFcn=@exportFigure, Separator="on");
+    Separator="on", ...
+    MenuSelectedFcn={@exportPolarPlot, plotAxis});
 uimenu(copyMenu, ...
     Text="Export Structure Definition", ...
-    MenuSelectedFcn=@exportStructure);
+    MenuSelectedFcn={@exportStructure, structureAxis});
 uimenu(copyMenu, ...
     Text="Export Both", ...
-    MenuSelectedFcn=@exportFigureAndStructure);
+    MenuSelectedFcn={@exportPolarPlotAndStructure, plotAxis, structureAxis});
 
 end
 
 
 
 %% Handler Functions
-function copyFigure(fig, ~)
-    handles = guidata(fig.Parent.Parent);
-    copygraphics(handles.plotAxis);
+function copyPolarPlot(~, ~, plotAxis)
+    copygraphics(plotAxis, ...
+        Resolution=200, ...
+        ContentType="image");
 end
 
-function copyStructure(fig, ~)
-    handles = guidata(fig.Parent.Parent);
-    copygraphics(handles.structureAxis);
+function copyStructure(~, ~, structureAxis)
+    copygraphics(structureAxis, ...
+        Resolution=200, ...
+        ContentType="image");
 end
 
-function copyFigureAndStructure(fig, ~)
-    handles = guidata(fig.Parent.Parent);
-    copygraphics(handles.plotPanel);
+function copyPolarPlotAndStructure(~, ~, plotAxis)
+    copygraphics(plotAxis.Parent, ...
+        Resolution=200, ...
+        ContentType="image");
 end
 
-function exportFigure(fig, ~)
-    handles = guidata(fig.Parent.Parent);
-    newFig = figure;
-    newPlotAxis = copyobj(handles.plotAxis, newFig);
-    newPlotAxis.Position = [0.13, 0.11, 0.775, 0.815];
 
-    if isprop(handles.plotAxis, "Legend")
+function exportPolarPlot(~, ~, plotAxis)
+    newFig = figure();
+    newPlotAxis = copyobj(plotAxis, newFig);
+    newPlotAxis.Position = [0.1300 0.1100 0.7750 0.8150];
+
+    if isprop(plotAxis, "Legend")
         legend(newPlotAxis);
     end
 end
 
-function exportStructure(fig, ~)
-    handles = guidata(fig.Parent.Parent);
+function exportStructure(~, ~, structureAxis)
     newFig = figure();
-    newStructureAxis = copyobj(handles.structureAxis, newFig);
+    newStructureAxis = copyobj(structureAxis, newFig);
     newStructureAxis.Children(1).FontSize = 9;
     newStructureAxis.Position = [0.1, 0.1, 0.8, 0.8];
 end
 
-function exportFigureAndStructure(fig, ~)
-    handles = guidata(fig.Parent.Parent);
-    newFig = figure(Position=[680, 200, 560, 600]);
-    newPlotAxis = copyobj(handles.plotAxis, newFig);
-    newStructureAxis = copyobj(handles.structureAxis, newFig);
+function exportPolarPlotAndStructure(~, ~, plotAxis, structureAxis)
+    plotPanel = plotAxis.Parent;
+    plotPanel.Units = "pixels";
+
+    newFig = figure(Position=plotPanel.Position);
+
+    newPlotAxis = copyobj(plotAxis, newFig);
+    newStructureAxis = copyobj(structureAxis, newFig);
     newStructureAxis.Children(1).FontSize = 9;
 
-    if isprop(handles.plotAxis, "Legend")
+    if isprop(plotAxis, "Legend")
         legend(newPlotAxis);
     end
 end
